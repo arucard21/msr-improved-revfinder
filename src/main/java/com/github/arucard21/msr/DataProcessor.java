96f1b1ab1885099e12c5fefb9a518bc6654c7b8f
arucard21 <arucard21@gmail.com>
2017-10-05 21:33:18 +0200

    Data is now parsed entirely, and cleaned up the code structure

--- src/main/java/com/github/arucard21/msr/DataProcessor.java
+++ src/main/java/com/github/arucard21/msr/DataProcessor.java
@@ -1,64 +1,134 @@
 package com.github.arucard21.msr;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
-
 import javax.json.Json;
-import javax.json.JsonArray;
+import javax.json.JsonObject;
 import javax.json.stream.JsonParser;
 
 public class DataProcessor {
 	
-	private static JsonParser androidChanges;
-	private static JsonArray chromiumChanges;
-	private static JsonArray openstackChanges;
-	private static JsonArray qtChanges;
-	
-	public static void main(String[] args) {
-		loadChanges();
-		System.out.printf("Collected %d changes from Gerrit for Android\n", jsonArraySize(androidChanges));
-//		System.out.printf("Collected %d changes from Gerrit for Chromium\n", chromiumChanges.size());
-//		System.out.printf("Collected %d changes from Gerrit for OpenStack\n", openstackChanges.size());
-//		System.out.printf("Collected %d changes from Gerrit for Qt\n", qtChanges.size());
+	public JsonObject showFirstObject(JsonParser parser) {
+		while(parser.hasNext()) {
+			switch(parser.next()) {
+				case START_ARRAY: 
+					break;
+				case START_OBJECT: 
+					return parser.getObject();
+				case KEY_NAME: 
+					break;
+				case VALUE_STRING: 
+					break;
+				case VALUE_NUMBER: 
+					break;
+				case VALUE_TRUE: 
+					break;
+				case VALUE_FALSE: 
+					break;
+				case VALUE_NULL: 
+					break;
+				case END_OBJECT: 
+					break;
+				case END_ARRAY: 
+					break;
+				default: 
+					break;
+			}
+		}
+		return null;
 	}
 
-	private static long jsonArraySize(JsonParser parser) {	
-		parser.next();
-		return parser.getArrayStream().count();
+	public JsonParser getAndroidChanges() {
+		try {
+			return Json.createParser(new FileReader(getResourceFile("android_changes.json")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public JsonParser getChromiumChanges() {
+		try {
+			return Json.createParser(new FileReader(getResourceFile("chromium_changes.json")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public JsonParser getOpenstackChanges() {
+		try {
+			return Json.createParser(new FileReader(getResourceFile("openstack_changes.json")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public JsonParser getQtChanges() {
+		try {
+			return Json.createParser(new FileReader(getResourceFile("qt_changes.json")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	/**
-	 * Uses {@link JsonReader} to load the JSON files into memory
+	 * Count the amount of objects in the first array you find with the given parser.
 	 * 
-	 * If the files turn out to be too large, we may need to switch to JsonParser in a streaming way.
-	 *  
-	 * @throws FileNotFoundException
+	 * @param parser for the JSON data
+	 * @return count of objects in the array
 	 */
-	private static void loadChanges() {
+	public int objectsInArray(JsonParser parser) {
+		int count = 0;
+		int arrayDepth = 0;
+		while(parser.hasNext()) {
+			switch(parser.next()) {
+				case START_ARRAY: 
+					arrayDepth++;
+					break;
+				case START_OBJECT: 
+					if (arrayDepth == 1) {
+						count++;
+					}
+					break;
+				case KEY_NAME: 
+					break;
+				case VALUE_STRING: 
+					break;
+				case VALUE_NUMBER: 
+					break;
+				case VALUE_TRUE: 
+					break;
+				case VALUE_FALSE: 
+					break;
+				case VALUE_NULL: 
+					break;
+				case END_OBJECT: 
+					break;
+				case END_ARRAY: 
+					arrayDepth--;
+					break;
+				default: 
+					break;
+			}
+		}
+		return count;
+	}
+	
+	private File getResourceFile(String filename) {
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		URL resource = classloader.getResource(filename);
 		try {
-			androidChanges = Json.createParser(new FileReader(Paths.get(getResourceFile("android_changes.json").toURI()).toFile()));
-//			chromiumChanges = Json.createParser(new FileReader(Paths.get(getResourceFile("chromium_changes.json").toURI()).toFile())).getArray();
-//			openstackChanges = Json.createParser(new FileReader(Paths.get(getResourceFile("openstack_changes.json").toURI()).toFile())).getArray();
-//			qtChanges = Json.createParser(new FileReader(Paths.get(getResourceFile("qt_changes.json").toURI()).toFile())).getArray();
-		} catch (FileNotFoundException e) {
-			System.err.println("File containing Gerrit changes could not be found");
-			e.printStackTrace();
+			return Paths.get(resource.toURI()).toFile();
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static URL getResourceFile(String filename) {
-		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-		URL resource = classloader.getResource(filename);
-		return resource;
+		return null;
 	}
 }
