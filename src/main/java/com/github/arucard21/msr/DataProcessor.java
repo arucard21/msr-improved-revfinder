38f251ef0c8e08834c316bc6306ef7d1d38a7c92
arucard21 <arucard21@gmail.com>
2017-10-06 09:45:47 +0200

    Make data processor use multiple files as source for each project

--- src/main/java/com/github/arucard21/msr/DataProcessor.java
+++ src/main/java/com/github/arucard21/msr/DataProcessor.java
@@ -5,127 +5,54 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.stream.JsonParser;
 
 public class DataProcessor {
-	
-	public JsonObject showFirstObject(JsonParser parser) {
-		while(parser.hasNext()) {
-			switch(parser.next()) {
-				case START_ARRAY: 
-					break;
-				case START_OBJECT: 
-					return parser.getObject();
-				case KEY_NAME: 
-					break;
-				case VALUE_STRING: 
-					break;
-				case VALUE_NUMBER: 
-					break;
-				case VALUE_TRUE: 
-					break;
-				case VALUE_FALSE: 
-					break;
-				case VALUE_NULL: 
-					break;
-				case END_OBJECT: 
-					break;
-				case END_ARRAY: 
-					break;
-				default: 
-					break;
+	public int countChangesForAndroid() {
+		int totalCount = 0;
+		int i = 0;
+		while(getResourceFile("android_changes_"+i+".json") != null) {
+			try {
+				JsonParser parser = Json.createParser(new FileReader(getResourceFile("android_changes_"+i+".json")));
+				parser.next();
+				totalCount+=parser.getArrayStream().count();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
 			}
+			i++;
 		}
-		return null;
-	}
-
-	public JsonParser getAndroidChanges() {
-		try {
-			return Json.createParser(new FileReader(getResourceFile("android_changes.json")));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public JsonParser getChromiumChanges() {
-		try {
-			return Json.createParser(new FileReader(getResourceFile("chromium_changes.json")));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public JsonParser getOpenstackChanges() {
-		try {
-			return Json.createParser(new FileReader(getResourceFile("openstack_changes.json")));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public JsonParser getQtChanges() {
-		try {
-			return Json.createParser(new FileReader(getResourceFile("qt_changes.json")));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	/**
-	 * Count the amount of objects in the first array you find with the given parser.
-	 * 
-	 * @param parser for the JSON data
-	 * @return count of objects in the array
-	 */
-	public int objectsInArray(JsonParser parser) {
-		int count = 0;
-		int arrayDepth = 0;
-		while(parser.hasNext()) {
-			switch(parser.next()) {
-				case START_ARRAY: 
-					arrayDepth++;
-					break;
-				case START_OBJECT: 
-					if (arrayDepth == 1) {
-						count++;
-					}
-					arrayDepth++;
-					break;
-				case KEY_NAME: 
-					break;
-				case VALUE_STRING: 
-					break;
-				case VALUE_NUMBER: 
-					break;
-				case VALUE_TRUE: 
-					break;
-				case VALUE_FALSE: 
-					break;
-				case VALUE_NULL: 
-					break;
-				case END_OBJECT:
-					arrayDepth--;
-					break;
-				case END_ARRAY: 
-					arrayDepth--;
-					break;
-				default: 
-					break;
-			}
-		}
-		return count;
+		return totalCount;
+		
 	}
 	
+	public int countChangesForOpenStack() {
+		int totalCount = 0;
+		int i = 0;
+		while(getResourceFile("openstack_changes_"+i+".json") != null) {
+			try {
+				JsonParser parser = Json.createParser(new FileReader(getResourceFile("openstack_changes_"+i+".json")));
+				parser.next();
+				totalCount+=parser.getArrayStream().count();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			i++;
+		}
+		return totalCount;
+		
+	}
+
 	private File getResourceFile(String filename) {
 		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 		URL resource = classloader.getResource(filename);
+		if (resource == null) {
+			return null;
+		}
 		try {
 			return Paths.get(resource.toURI()).toFile();
 		} catch (URISyntaxException e) {
--- src/main/java/com/github/arucard21/msr/DataProcessorApplication.java
+++ src/main/java/com/github/arucard21/msr/DataProcessorApplication.java
@@ -1,29 +1,11 @@
 package com.github.arucard21.msr;
 
-import java.util.HashMap;
-
-import javax.json.Json;
-import javax.json.JsonWriter;
-import javax.json.stream.JsonGenerator;
-
 public class DataProcessorApplication {
-	private static JsonWriter writer;
 
 	public static void main(String[] args) {
-		configurePrettyPrintWriterToSysOut();
 		DataProcessor processor = new DataProcessor();
-		System.out.printf("Collected %d changes from Gerrit for Android\n", processor.objectsInArray(processor.getAndroidChanges()));
-		System.out.printf("Collected %d changes from Gerrit for Chromium\n", processor.objectsInArray(processor.getChromiumChanges()));
-		System.out.printf("Collected %d changes from Gerrit for OpenStack\n", processor.objectsInArray(processor.getOpenstackChanges()));
-		System.out.printf("Collected %d changes from Gerrit for Qt\n", processor.objectsInArray(processor.getQtChanges()));
+		System.out.printf("Collected %d changes from Gerrit for Android\n", processor.countChangesForAndroid());
+		System.out.printf("Collected %d changes from Gerrit for OpenStack\n", processor.countChangesForOpenStack());
 		
-		writer.write(processor.showFirstObject(processor.getAndroidChanges()));
-	}
-
-	private static JsonWriter configurePrettyPrintWriterToSysOut() {
-		HashMap<String, Object> config = new HashMap<>();
-		config.put(JsonGenerator.PRETTY_PRINTING, true);
-		writer = Json.createWriterFactory(config).createWriter(System.out);
-		return writer;
 	}
 }
