16d8de0182cb2d443c0a45799c0d4881ca0f7c17
Johannes Zenz <johannes.zenz@student.tugraz.at>
2017-10-06 13:01:04 +0200

    add counting of Chromium & OpenStack

--- src/main/java/com/github/arucard21/msr/DataProcessor.java
+++ src/main/java/com/github/arucard21/msr/DataProcessor.java
@@ -5,15 +5,55 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.json.Json;
-import javax.json.JsonObject;
 import javax.json.stream.JsonParser;
 
 public class DataProcessor {
-	public int countChangesForAndroid() {
+
+    public enum PROJECT {ANDROID, CHROMIUM, OPENSTACK, QT}
+
+    public String getProjectName(PROJECT name) {
+        switch (name)
+        {
+            case ANDROID:
+                return "android";
+
+            case CHROMIUM:
+                return "chromium";
+
+            case OPENSTACK:
+                return "openstack";
+
+            case QT:
+                return "qt";
+
+            default:
+                return "error";
+        }
+    }
+
+    public int countChangesForProject(PROJECT project) {
+        int totalCount = 0;
+        int i = 0;
+        String projectName = getProjectName(project);
+        String filename = project + "_changes_"+i+".json";
+        while(getResourceFile(filename) != null) {
+            try {
+                JsonParser parser = Json.createParser(new FileReader(getResourceFile(filename)));
+                parser.next();
+                totalCount+=parser.getArrayStream().count();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            i++;
+            filename = project + "_changes_"+i+".json";
+        }
+        return totalCount;
+    }
+
+    /*
+    public int countChangesForAndroid() {
 		int totalCount = 0;
 		int i = 0;
 		while(getResourceFile("android_changes_"+i+".json") != null) {
@@ -27,7 +67,6 @@ public class DataProcessor {
 			i++;
 		}
 		return totalCount;
-		
 	}
 	
 	public int countChangesForOpenStack() {
@@ -46,6 +85,7 @@ public class DataProcessor {
 		return totalCount;
 		
 	}
+    */
 
 	private File getResourceFile(String filename) {
 		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
--- src/main/java/com/github/arucard21/msr/DataProcessorApplication.java
+++ src/main/java/com/github/arucard21/msr/DataProcessorApplication.java
@@ -4,8 +4,13 @@ public class DataProcessorApplication {
 
 	public static void main(String[] args) {
 		DataProcessor processor = new DataProcessor();
-		System.out.printf("Collected %d changes from Gerrit for Android\n", processor.countChangesForAndroid());
-		System.out.printf("Collected %d changes from Gerrit for OpenStack\n", processor.countChangesForOpenStack());
-		
+
+		System.out.printf("Collected %d changes from Gerrit for Android\n", processor.countChangesForProject(DataProcessor.PROJECT.ANDROID));
+		System.out.printf("Collected %d changes from Gerrit for Chromium\n", processor.countChangesForProject(DataProcessor.PROJECT.CHROMIUM));
+		System.out.printf("Collected %d changes from Gerrit for OpenStack\n", processor.countChangesForProject(DataProcessor.PROJECT.OPENSTACK));
+		System.out.printf("Collected %d changes from Gerrit for Qt\n", processor.countChangesForProject(DataProcessor.PROJECT.QT));
+
+		//System.out.printf("Collected %d changes from Gerrit for Android\n", processor.countChangesForAndroid());
+		//System.out.printf("Collected %d changes from Gerrit for OpenStack\n", processor.countChangesForOpenStack());
 	}
 }
