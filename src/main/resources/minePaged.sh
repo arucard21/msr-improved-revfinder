7f2c83c02b14c568539ad3f42057db7c73434130
arucard21 <arucard21@gmail.com>
2017-10-06 07:57:52 +0200

    Fix Qt data retrieval (uses older API with different paging)

--- src/main/resources/minePaged.sh
+++ src/main/resources/minePaged.sh
@@ -14,9 +14,14 @@ do
 	then
 		curl "https://review.openstack.org/changes/?o=ALL_REVISIONS&o=ALL_FILES&o=ALL_COMMITS&o=MESSAGES&o=DETAILED_ACCOUNTS&S=$(expr $i \* $pageSize)&n=${pageSize}" -o "openstack_changes_${i}.json"
 	fi
-	if [ $i -eq 0 ] || ( [ -f "qt_changes_$(expr $i - 1).json" ] && grep --quiet "_more_changes" "qt_changes_$(expr $i - 1).json" )
-	then
-		curl "https://codereview.qt-project.org/changes/?o=ALL_REVISIONS&o=ALL_FILES&o=ALL_COMMITS&o=MESSAGES&o=DETAILED_ACCOUNTS&S=$(expr $i \* $pageSize)&n=${pageSize}" -o "qt_changes_${i}.json"
+	if [ $i -eq 0 ] 
+	then 
+		curl "https://codereview.qt-project.org/changes/?o=ALL_REVISIONS&o=ALL_FILES&o=ALL_COMMITS&o=MESSAGES&o=DETAILED_ACCOUNTS&n=${pageSize}" -o "qt_changes_${i}.json"
+	else
+		if [ -f "qt_changes_$(expr $i - 1).json" ] && grep --quiet "_more_changes" "qt_changes_$(expr $i - 1).json"
+		then
+			sortkey=$(grep -Eo "\"_sortkey\": \".*\"" "qt_changes_$(expr $i - 1).json"| tail -1 | sed "s/\"_sortkey\": \"\(.*\)\"/\1/")
+			curl "https://codereview.qt-project.org/changes/?o=ALL_REVISIONS&o=ALL_FILES&o=ALL_COMMITS&o=MESSAGES&o=DETAILED_ACCOUNTS&N=${sortkey}&n=${pageSize}" -o "qt_changes_${i}.json"
+		fi
 	fi
-	sleep 1
 done
