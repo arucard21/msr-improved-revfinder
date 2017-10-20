a1d5d9a744e3479a0083b45065185c7259a33c8c
arucard21 <arucard21@gmail.com>
2017-10-20 09:24:04 +0200

    Added latest amounts as comments in the code for easy lookup

--- src/main/java/com/github/arucard21/msr/DataProcessorApplication.java
+++ src/main/java/com/github/arucard21/msr/DataProcessorApplication.java
@@ -7,10 +7,17 @@ public class DataProcessorApplication {
 	public static void main(String[] args) throws IOException {
 		DataProcessor processor = new DataProcessor();
 
-//		System.out.printf("Collected %d changes from Gerrit for Android\n", processor.countChangesForProject(Project.ANDROID));
-//		System.out.printf("Collected %d changes from Gerrit for Chromium\n", processor.countChangesForProject(Project.CHROMIUM));
-//		System.out.printf("Collected %d changes from Gerrit for OpenStack\n", processor.countChangesForProject(Project.OPENSTACK));
-//		System.out.printf("Collected %d changes from Gerrit for Qt\n", processor.countChangesForProject(Project.QT));
+		System.out.printf("Collected %d changes from Gerrit for Android\n", processor.countChangesForProject(Project.ANDROID));
+		System.out.printf("Collected %d changes from Gerrit for Chromium\n", processor.countChangesForProject(Project.CHROMIUM));
+		System.out.printf("Collected %d changes from Gerrit for OpenStack\n", processor.countChangesForProject(Project.OPENSTACK));
+		System.out.printf("Collected %d changes from Gerrit for Qt\n", processor.countChangesForProject(Project.QT));
+		/*
+		 * Latest Output:
+			Collected 3500 changes from Gerrit for Android
+			Collected 77185 changes from Gerrit for Chromium
+			Collected 493548 changes from Gerrit for OpenStack
+			Collected 180092 changes from Gerrit for Qt
+		 */
 		
 		processor.filter();
 		
@@ -18,5 +25,12 @@ public class DataProcessorApplication {
 		System.out.printf("Filtered %d changes for Chromium\n", processor.countFilteredChangesForProject(Project.CHROMIUM));
 		System.out.printf("Filtered %d changes for OpenStack\n", processor.countFilteredChangesForProject(Project.OPENSTACK));
 		System.out.printf("Filtered %d changes for Qt\n", processor.countFilteredChangesForProject(Project.QT));
+		/*
+		 * Latest Output:
+			Filtered 0 changes for Android
+			Filtered 26 changes for Chromium
+			Filtered 5966 changes for OpenStack
+			Filtered 24789 changes for Qt
+		 */
 	}
 }
