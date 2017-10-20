b1991ca1b88a7a7dd0b8c9a6a02cd7947b0d99bd
arucard21 <arucard21@gmail.com>
2017-10-20 11:44:00 +0200

    Added new scripts for data retrieval from Android and Chromium

---
+++ src/main/data/raw/chromium/mineChromium.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+projectsFile="projectURLs.txt"
+refs="chromium_refs.txt"
+changeNumbers="chromium_changeNumbers.txt"
+noReview="chromium_noReview.txt"
+retrieved="chromium_retrieved.txt"
+changes="chromium_changes_all.json"
+response="chromium_response.json"
+
+# curl "https://chromium-review.googlesource.com/projects/" | grep "\"url\"\: " | sed "s/^.*\"url\"\: \"https:\/\/chromium.googlesource.com\/\(.*\)\/.*$/\https:\/\/chromium.googlesource.com\/\1.git/" > $projectsFile
+# 
+# while read -r gitURL
+# do 
+# 	git ls-remote $gitURL | grep refs/changes/ >> $refs 
+# 	sleep 500ms
+# done < $projectsFile
+# 
+# sed "s/^.*refs\/changes\/[0-9]\+\/\([0-9]\+\)\/.*$/\1/" $refs | uniq > $changeNumbers
+
+if [ ! -f $noReview ]
+then 
+	touch $noReview
+fi
+
+if [ ! -f $retrieved ]
+then 
+	touch $retrieved
+fi
+
+count=0
+while read -r changeNumber
+do 
+	if [ ! $(grep "^$changeNumber$" $noReview) ]
+	then 
+		if [ ! $(grep "^$changeNumber$" $retrieved) ]
+		then
+			if curl --output $response --silent --fail "https://chromium-review.googlesource.com/changes/${changeNumber}?o=ALL_REVISIONS&o=ALL_FILES&o=ALL_COMMITS&o=MESSAGES&o=DETAILED_ACCOUNTS"
+			then
+				if [ -s $changes ] 
+				then
+					echo "," >> $changes
+				fi
+				echo $changeNumber >> $retrieved
+				cat $response | sed "/)]}'/d" >> $changes
+			else
+				echo $changeNumber >> $noReview
+			fi
+			sleep 0.2s
+		fi
+	fi
+	echo "Amount processed: ${count}"
+	count=$(expr $count + 1)
+done < $changeNumbers
+
+# add characters to make it a valid JSON array
+sed -i "1i \[" $changes
+echo "]" >> $changes
+rm $response
