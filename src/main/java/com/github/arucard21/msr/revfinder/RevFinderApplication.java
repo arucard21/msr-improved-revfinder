c3411d31db3a0f442e9283c07527435414cca24e
arucard21 <arucard21@gmail.com>
2017-11-01 09:55:14 +0100

    Made revfinder generate recommendations for each project in a separate thread

--- src/main/java/com/github/arucard21/msr/revfinder/RevFinderApplication.java
+++ src/main/java/com/github/arucard21/msr/revfinder/RevFinderApplication.java
@@ -4,26 +4,31 @@ import com.github.arucard21.msr.Project;
 
 public class RevFinderApplication {
 	public static void main(String[] args) {
-//		for (Project project : Project.values()) {
-		Project project = Project.OPENSTACK;
+		for (Project project : Project.values()) {
 			// you can skip projects by making sure the project_recommendations.json file already exists in the revfinder folder
 			RevFinder revFinder = new RevFinder(project );
-			revFinder.generateReviewerRecommendations(false);
-			revFinder.generateReviewerRecommendations(true);
-			System.out.println("Results for data filtered only on created date:");
-			System.out.println(project.name+" top-k accuracy for first = "+revFinder.calculateTopKAccuracy(10, false, false));
-			System.out.println(project.name+" top-k accuracy for last = "+revFinder.calculateTopKAccuracy(10, true, false));
-			System.out.println(project.name+" MRR for first = "+revFinder.calculateMRR(false, false));
-			System.out.println(project.name+" MRR for last = "+revFinder.calculateMRR(true, false));
-			System.out.println(project.name+" average number of files = "+revFinder.getAverageNumberFiles(false));
-			System.out.println("");
-			System.out.println("Results for more filtered data:");
-			System.out.println(project.name+" top-k accuracy for first = "+revFinder.calculateTopKAccuracy(10, false, true));
-			System.out.println(project.name+" top-k accuracy for last = "+revFinder.calculateTopKAccuracy(10, true, true));
-			System.out.println(project.name+" MRR for first = "+revFinder.calculateMRR(false, true));
-			System.out.println(project.name+" MRR for last = "+revFinder.calculateMRR(true, true));
-			System.out.println(project.name+" average number of files = "+revFinder.getAverageNumberFiles(true));
-//		}
+			new Thread(() -> {
+				System.out.printf("[%s based-on-created] Generating recommendations\n", project.name);
+				revFinder.generateReviewerRecommendations(false);
+				System.out.printf("[%s based-on-created] Finished generating recommendations\n", project.name);
+				System.out.printf("[%s based-on-created] top-k accuracy for first = %f\n", project.name, revFinder.calculateTopKAccuracy(10, false, false));
+				System.out.printf("[%s based-on-created] top-k accuracy for last = %f\n", project.name, revFinder.calculateTopKAccuracy(10, true, false));
+				System.out.printf("[%s based-on-created] MRR for first = %f\n", project.name, revFinder.calculateMRR(false, false));
+				System.out.printf("[%s based-on-created] MRR for last = %f\n", project.name, revFinder.calculateMRR(true, false));
+				System.out.printf("[%s based-on-created] average number of files = %f\n", project.name, revFinder.getAverageNumberFiles(false));
+			}).start();
+			
+			new Thread(() -> {
+				System.out.printf("[%s within-period] Generating recommendations\n", project.name);
+				revFinder.generateReviewerRecommendations(true);
+				System.out.printf("[%s within-period] Finished generating recommendations\n", project.name);
+				System.out.printf("[%s within-period] top-k accuracy for first = %f\n", project.name, revFinder.calculateTopKAccuracy(10, false, true));
+				System.out.printf("[%s within-period] top-k accuracy for last = %f\n", project.name, revFinder.calculateTopKAccuracy(10, true, true));
+				System.out.printf("[%s within-period] MRR for first = %f\n", project.name, revFinder.calculateMRR(false, true));
+				System.out.printf("[%s within-period] MRR for last = %f\n", project.name, revFinder.calculateMRR(true, true));
+				System.out.printf("[%s within-period] average number of files = %f\n", project.name, revFinder.getAverageNumberFiles(true));
+			}).start();
+		}
 
 		/*
 		Generated recommended reviewers for openstack
